907d3a3da02cbae9b49ceace43b396424d2df1c0
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-10-04 21:19:43 +0300

    asf

--- src/routes/article.route.ts
+++ src/routes/article.route.ts
@@ -2,11 +2,6 @@ import { FastifyInstance } from 'fastify';
 import { articleController } from '../controllers';
 
 const articleRouter = async (app: FastifyInstance) => {
-  const article = {
-    id: "1",
-    name: "node.js fastify",
-    desc: "описание и тест статей"
-  }
 
   app.get("/", articleController.handleGetArticle)
 
